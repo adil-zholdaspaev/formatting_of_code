76b6044d5a27a0b035994315acd254fc842841ec
Adil Zholdaspaev <azholdaspaev@thumbtack.net>
2015-09-18 16:56:15 +0600

    Status getter

--- src/main/java/net/omsu/formatter/formatter/Status.java
+++ src/main/java/net/omsu/formatter/formatter/Status.java
@@ -10,5 +10,16 @@ public enum Status {
     SPACE,
     OPEN_BRACE,
     CLOSE_BRACE,
-    SEMICOLON
+    SEMICOLON;
+
+    public static Status getStatus(final char character) {
+        switch (character) {
+            case ' ' : return SPACE;
+            case ';' : return SEMICOLON;
+            case '{' : return OPEN_BRACE;
+            case '}' : return CLOSE_BRACE;
+            case '\n': return NEW_LINE;
+            default  : return CHAR;
+        }
+    }
 }
