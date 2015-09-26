42f3bbbf4df46b73a5305e11e0053b6b012f7f4c
azholdaspaev <adilzholdaspaev@gmail.com>
2015-09-26 23:52:57 +0600

    Changed implementation of FileReader

--- src/main/java/net/omsu/formatter/reader/FileReader.java
+++ src/main/java/net/omsu/formatter/reader/FileReader.java
@@ -5,20 +5,16 @@ import net.omsu.formatter.exception.ReaderException;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.Queue;
 
 /**
  *
  */
 public class FileReader implements Reader {
 
-    // bufferSize may be configured
-    private static final int bufferSize = 10;
-
     private final BufferedReader bufferedReader;
-    private Queue<Character> characterBuffer;
-    private boolean isEndOfFile = false;
+    private Character currentCharacter;
+    private Character nextCharacter;
+    private boolean isReaderClosed = false;
 
     public FileReader(final String fileName) throws ReaderException {
         if (fileName == null) {
@@ -28,53 +24,49 @@ public class FileReader implements Reader {
         try {
             bufferedReader = new BufferedReader(new java.io.FileReader(fileName));
         } catch (FileNotFoundException ex) {
-            throw new ReaderException(String.format("File with name %s not found", fileName), ex);
+            throw new ReaderException(String.format("File %s can't be opened", fileName), ex);
         }
 
-        characterBuffer = new LinkedList<>();
-        fillBuffer();
+        currentCharacter = readNext();
+        nextCharacter = readNext();
     }
 
     @Override
     public char read() throws ReaderException {
-        char currentCharacter = characterBuffer.poll();
-        if (!hasNext()) {
-            fillBuffer();
+        if (isReaderClosed || !hasNext()) {
+            return (char) -1;
         }
+        char result = currentCharacter;
+        currentCharacter = nextCharacter;
+        nextCharacter = readNext();
 
-        return currentCharacter;
+        return result;
     }
 
     @Override
     public boolean hasNext() {
-        return !characterBuffer.isEmpty();
+        return currentCharacter != null;
     }
 
     @Override
     public void close() throws ReaderException {
-        isEndOfFile = true;
+        isReaderClosed = true;
         try {
             bufferedReader.close();
         } catch (IOException ex) {
-            throw new ReaderException(String.format("Can not close reader"), ex);
+            throw new ReaderException("Can't close the file reader", ex);
         }
     }
 
-    private void fillBuffer() throws ReaderException {
-        if (hasNext() || isEndOfFile) {
-            return;
-        }
-        for (int i = 0; i < bufferSize; i++) {
-            try {
-                int characterCode = bufferedReader.read();
-                if (characterCode == -1) {
-                    isEndOfFile = true;
-                    return;
-                }
-                characterBuffer.add((char) characterCode);
-            } catch (IOException ex) {
-                throw new ReaderException(String.format("Can't read character from file"), ex);
+    private Character readNext() throws ReaderException {
+        try {
+            int characterCode = bufferedReader.read();
+            if (characterCode == -1) {
+                return null;
             }
+            return (char) characterCode;
+        } catch (IOException ex) {
+            throw new ReaderException("Can't read character from file", ex);
         }
     }
 }
